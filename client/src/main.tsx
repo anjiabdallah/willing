c425e2853f0eafc5a048da303a44a3eae233f0a4
MarcHamamji <hamamjimarc@gmail.com>
2026-01-30 22:59:01 +0200

    Setup client

---
+++ client/src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import AdminPage from './pages/AdminPage';
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/admin',
+    element: <AdminPage />
+  }
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)

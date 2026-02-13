e451c3fc13e11b06256dbfcc51dfe514039505a0
LaraElKhatib <lara.m.elkhatib@gmail.com>
2026-02-13 11:47:13 +0200

    Check that the emails used in the organization requests are not already present in the organization account table or the organization request table

--- server/src/api/routes/organization.ts
+++ server/src/api/routes/organization.ts
@@ -18,11 +18,41 @@ const orgRequestSchema = zod.object({
 orgRouter.post('/request', async (req, res) => {
   const body = orgRequestSchema.parse(req.body);
 
+  const email = body.email.toLowerCase().trim();
+
+  // check email in existing account
+  const checkAccountRequest = await database
+    .selectFrom('organization_account')
+    .select('id')
+    .where('email', '=', email)
+    .executeTakeFirst();
+
+  if (checkAccountRequest) {
+    return res.status(400).json({
+      success: false,
+      message: 'An organization with this email account already exists',
+    });
+  }
+
+  // check email in pending requests
+  const checkPendingRequest = await database
+    .selectFrom('organization_request')
+    .select('id')
+    .where('email', '=', email)
+    .executeTakeFirst();
+
+  if (checkPendingRequest) {
+    return res.status(400).json({
+      success: false,
+      message: 'A request with this email is already pending',
+    });
+  }
+
   const organization = await database
     .insertInto('organization_request')
     .values({
       name: body.name,
-      email: body.email,
+      email: email,
       phone_number: body.phone_number,
       url: body.url,
       location_name: body.location_name,

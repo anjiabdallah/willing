ad9ebcaff3eff0e699722afd8aa7fce52c9e72a1
anjiabdallah <anjiabdallah@gmail.com>
2026-02-16 20:48:14 +0200

    delete organization.ts completely

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
-import { setUserJWT } from './authorization.js';
+import { setUserJWT, authorizeOnly } from './authorization.js';
 import adminRouter from './routes/admin/index.js';
-import orgRouter from './routes/organization.js';
+import indexRouter from './routes/organization/organizationIndex.js';
+import postingRouter from './routes/organization/organizationPosting.js';
 import userRouter from './routes/user.js';
 import volunteerRouter from './routes/volunteer.js';
 
@@ -12,6 +13,9 @@ api.use(setUserJWT);
 api.use('/user', userRouter);
 api.use('/admin', adminRouter);
 api.use('/volunteer', volunteerRouter);
-api.use('/organization', orgRouter);
+
+// Organization routes: public routes first, then protected routes
+api.use('/organization', indexRouter);
+api.use('/organization', authorizeOnly('organization'), postingRouter);
 
 export default api;
